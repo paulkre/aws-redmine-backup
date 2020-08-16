bce3f189b0ace66792c07522a846a906ee7a6272
Paul Kretschel <paul@kretschel.media>
2020-08-17 00:19:23 +0200

    Handle errors on server side

--- app/src/ssh.ts
+++ app/src/ssh.ts
@@ -1,16 +1,30 @@
 import { NodeSSH } from "node-ssh";
 import { Config } from "./config";
 
+type SSH = {
+  execCommand(command: string): Promise<string>;
+  dispose(): void;
+};
+
 export async function createSSH({
   sshHost,
   sshUser,
   sshKey,
-}: Config): Promise<NodeSSH> {
-  const client = new NodeSSH();
-
-  return client.connect({
+}: Config): Promise<SSH> {
+  const client = await new NodeSSH().connect({
     host: sshHost,
     username: sshUser,
     privateKey: sshKey,
   });
+
+  return {
+    async execCommand(command) {
+      const { stdout, stderr } = await client.execCommand(command);
+
+      if (stderr) throw stderr;
+
+      return stdout;
+    },
+    dispose: () => client.dispose(),
+  };
 }
